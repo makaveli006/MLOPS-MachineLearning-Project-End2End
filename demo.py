d9dc5ffc55af6b659ed9c3c6b6a40bed0c1e6c51
Subin <subinkrishna006@gmail.com>
2025-12-04 17:24:05 +0530

    Add AWS S3 and MongoDB integration for US Visa prediction model
    - Implemented SimpleStorageService for S3 operations including file upload, download, and model management.
    - Created S3Client to manage AWS credentials and establish connections to S3.
    - Developed MongoDBClient for connecting to MongoDB and exporting data as DataFrame.
    - Added USvisaData class to facilitate data retrieval from MongoDB collections.
    - Introduced USvisaModel and USvisaEstimator classes for model prediction and management.
    - Created HTML template for US Visa prediction form and corresponding CSS for styling.
    - Added requirements_detailed.txt for project dependencies.

--- demo.py
+++ demo.py
@@ -1,13 +1,21 @@
 
-# #### logger submodule testing in demo.py ####
-from us_visa.logger import logging
-# logging.info("Demo file executed")
+# # #### logger submodule testing in demo.py ####
+# from us_visa.logger import logging
+# # logging.info("Demo file executed")
 
-###### exception submodule testing in demo.py ####
-from us_visa.exception import USvisaException
-import sys
+# ###### exception submodule testing in demo.py ####
+# from us_visa.exception import USvisaException
+# import sys
 
-try:
-    a = 1 / 0
-except Exception as e:
-    raise USvisaException(e, sys)
+# try:
+#     a = 1 / 0
+# except Exception as e:
+#     raise USvisaException(e, sys)
+# import os
+
+# print(os.getenv("MONGODB_URL"))
+
+from us_visa.pipline.training_pipeline import TrainPipeline
+
+obj = TrainPipeline()
+obj.run_pipeline()
